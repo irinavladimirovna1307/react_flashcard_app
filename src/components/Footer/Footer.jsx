e8687778e99d144ab4522aa4404b7b75f40aa41d
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-05-25 02:48:59 +0300

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import style from './footer.module.scss'
+import React from "react";
+import style from "./footer.module.scss";
 
 export default function Footer() {
-    return (
-        <div className={style.container}>
-            <div className={style.item}>
-                <img className={style.img} src='./assets/studying2.png' alt='logo'></img>
-                <span className={style.text}> Created by MZ-87 ⓒ 2022</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      <div className={style.item}>
+        <img
+          className={style.img}
+          src="./assets/studying2.png"
+          alt="logo"
+        ></img>
+        <span className={style.text}>
+          {" "}
+          Created by irinavladimirovna1307 ⓒ 2025
+        </span>
+      </div>
+    </div>
+  );
 }
